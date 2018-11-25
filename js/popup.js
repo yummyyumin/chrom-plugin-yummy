3b160be09507bf852089469620461ee59123dbd7
yummy <yummy@yummydeMacBook-Pro-2.local>
2018-11-25 14:44:02 +0800

    first commit

---
+++ js/popup.js
@@ -0,0 +1,18 @@
+onload=function(){
+    chrome.tabs.getSelected(function(tab){
+        //QRCode(元素id,相关配置文件)
+        var qrcode = new QRCode("qrcode", {
+                text: tab.url,
+                width: 160,
+                height: 160,
+                colorDark : '#000000',
+                colorLight : '#ffffff',
+                // QRCode的容错级别
+                correctLevel : QRCode.CorrectLevel.H
+                });
+        if (tab.favIconUrl) {//tab有图标的情况下动态赋值
+        var img = document.getElementsByTagName("img")[1].src = tab.favIconUrl;
+        }
+        console.log(img);
+    });
+}
