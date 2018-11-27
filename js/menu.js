35bb50e70e2f65ec8fbddb366d6272485d443f96
yummy <yummy@yummydeMacBook-Pro-2.local>
2018-11-27 17:22:48 +0800

    menu

--- js/menu.js
+++ js/menu.js
@@ -1,20 +1,20 @@
-
-function searchAddress(info){
+function searchAddress(info) {
     alert(info.selectionText);
-    let searchcode = encodeURIComponent(info.selectionText);
-    alert(searchcode);
-    var newURL = "https://www.google.cn/maps/search/"+searchcode+"?hl=zh-CN&source=opensearch";
-    chrome.tabs.create({ url: newURL });
+    var searchcode = encodeURIComponent(info.selectionText);
+    var newURL = "https://www.google.cn/maps/search/" + searchcode + "?hl=zh-CN&source=opensearch";
+    chrome.tabs.create({
+        url: newURL
+    });
 }
 
-chrome.contextMenus.create(
-    {   
-        "type" :"normal",
-        "title": "百度地图搜索此地址",
-        "contexts":["all"],
+// 右键增加选项
+chrome.contextMenus.create({
+        "type": "normal",
+        "title": "google地图搜索此地址",
+        "contexts": ["all"],
         "onclick": searchAddress
     },
-    function(){
+    function () {
         console.log("创建右菜单");
     }
 )
